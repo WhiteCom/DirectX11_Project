810178a743201d2a8e3f2cd472f2e194a2153ee3
WhiteCom <vip1408@naver.com>
2025-08-11 01:18:40 +0900

    Add DX11_TEST_04 (Mouse Position Triangles Moving using Constant Buffer)

---
+++ DX11_TEST_04/Game.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "CommonHeader.h"
+#include "Timer.h"
+#include <Mouse.h> // from DirectXTK Library
+
+class Game
+{
+public:
+	Game() noexcept;
+	~Game() noexcept;
+
+public:
+	HRESULT InitGame(HWND hWnd);
+	void DestroyGame();
+
+	void ClearBuffer();
+	void UpdateGame();
+	void RenderGame();
+
+	void DrawTestTriangle();
+
+	//void DrawTestTriangle2(float angle);
+	void DrawTestTriangle2(float angle, float x, float y);
+
+private:
+	Microsoft::WRL::ComPtr<ID3D11Device> m_d3dDevice;
+	Microsoft::WRL::ComPtr<IDXGISwapChain> m_swapChain;
+	Microsoft::WRL::ComPtr<ID3D11DeviceContext> m_d3dContext;
+	Microsoft::WRL::ComPtr<ID3D11RenderTargetView> m_renderTargetView;
+
+	Timer m_Timer;
+
+	std::unique_ptr<DirectX::Mouse> m_mouse;
+};
